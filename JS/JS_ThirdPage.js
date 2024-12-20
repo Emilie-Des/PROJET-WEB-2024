4aa3910cc824f530b8e37e7f08d09ec5a14e917a
Stacey <staceycodjia@gmail.com>
2024-12-20 20:40:48 +0100

    Améliorations CSS (menu)

--- JS/JS_ThirdPage.js
+++ JS/JS_ThirdPage.js
@@ -1,31 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuItems = document.querySelectorAll(".menu-container div");
 
-    // Map des catégories API pour Entrée, Plat et Dessert
+    
     const apiCategories = {
-        "entrée": ["Starter"], 
-        "plat": ["Chicken"], 
+        "entrée": "Starter", 
+        "plat": "Chicken", 
         "dessert": "Dessert"
     };
 
     menuItems.forEach((menuItem, index) => {
-        const categoryName = Object.keys(apiCategories)[index]; // entrée, plat, dessert
-        const apiCategory = apiCategories[categoryName]; // Catégorie API correspondante
+        const categoryName = Object.keys(apiCategories)[index]; 
+        const apiCategory = apiCategories[categoryName]; 
 
-        // Fetch de repas dans la catégorie correspondante
+        
         fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${apiCategory}`)
             .then(response => response.json())
             .then(data => {
-                // Choisir un repas aléatoire parmi les résultats
+                
                 const randomMeal = data.meals[Math.floor(Math.random() * data.meals.length)];
 
-                // Obtenir les détails du repas
+                
                 fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${randomMeal.idMeal}`)
                     .then(response => response.json())
                     .then(detailData => {
                         const meal = detailData.meals[0];
 
-                        // Mettre à jour l'image, le nom, et le lien YouTube
+                        
                         menuItem.querySelector("img").src = meal.strMealThumb;
                         menuItem.querySelector("p").textContent = meal.strMeal;
                         const link = menuItem.querySelector("a");
